5e767d3937e4a6eb8657f16777046ea9f63652b0
Luis Finke <lfinke@fusionalliance.com>
2019-08-11 16:12:54 -0500

    Initial Commit

---
+++ src/Types.hpp
@@ -0,0 +1,24 @@
+//
+//  Types.hpp
+//  AsyncCpp
+//
+//  Created by Luis Finke on 8/11/19.
+//  Copyright © 2019 Luis Finke. All rights reserved.
+//
+
+#pragma once
+
+#include <chrono>
+#include <functional>
+#include <list>
+#include <mutex>
+#include <string>
+
+namespace fgl {
+	template<typename T>
+	using Function = std::function<T>;
+	template<typename T>
+	using LinkedList = std::list<T>;
+	using Mutex = std::mutex;
+	using String = std::string;
+}
