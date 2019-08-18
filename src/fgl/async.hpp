269184a40dfea5b1e54145981c7c16aefdaf3b67
Luis Finke <lfinke@fusionalliance.com>
2019-08-17 23:16:53 -0500

    async.hpp

---
+++ src/fgl/async.hpp
@@ -0,0 +1,13 @@
+//
+//  async.hpp
+//  AsyncCpp
+//
+//  Created by Luis Finke on 8/17/19.
+//  Copyright © 2019 Luis Finke. All rights reserved.
+//
+
+#include "async/Common.hpp"
+#include "async/DispatchQueue.hpp"
+#include "async/DispatchTimeoutResult.hpp"
+#include "async/DispatchWorkItem.hpp"
+#include "async/Promise.hpp"
