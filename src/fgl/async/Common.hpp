29071db222ec5b93d7efe2e361b72d5f2c8c70b0
Luis Finke <lfinke@fusionalliance.com>
2019-11-23 14:58:45 -0500

    add optional to standalone definitions

--- src/fgl/async/Common.hpp
+++ src/fgl/async/Common.hpp
@@ -30,6 +30,19 @@ namespace fgl {
 		using LinkedList = std::list<T>;
 		using String = std::string;
 		using Any = std::any;
+
+		template<typename T>
+		using Optional = std::optional<T>;
+		template<typename T, bool isOptional = std::is_same<T, Optional<T>>::value>
+		struct optionalize_t {
+			using type = Optional<T>;
+		};
+		template<typename T>
+		struct optionalize_t<T,true> {
+			using type = T;
+		};
+		template<typename T>
+		using Optionalized = typename optionalize_t<T>::type;
 	#endif
 	
 	#ifndef FGL_ASSERT
