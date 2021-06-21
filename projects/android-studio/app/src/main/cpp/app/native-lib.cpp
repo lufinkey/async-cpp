13b0097152e9c3b09778f0741a1db1f4f96b1192
Luis Finke <lfinke@fusionalliance.com>
2021-06-21 19:12:02 -0400

    add required things

--- projects/android-studio/app/src/main/cpp/app/native-lib.cpp
+++ projects/android-studio/app/src/main/cpp/app/native-lib.cpp
@@ -1,10 +1,12 @@
 #include <jni.h>
 #include <string>
+#include <fgl/data.hpp>
 
 extern "C" JNIEXPORT jstring JNICALL
 Java_com_lufinkey_asynccpp_MainActivity_stringFromJNI(
 		JNIEnv *env,
 		jobject /* this */) {
 	std::string hello = "Hello from C++";
+	auto anyTest = fgl::Any();
 	return env->NewStringUTF(hello.c_str());
 }
